eb793b82abf4e51cb157dbf1341db44df42fb916
Sànu 🦋 <sanoops658@gmail.com>
2025-12-30 19:00:10 +0530

    MySQL DataBase Completed

--- controller.py
+++ controller.py
@@ -2,28 +2,73 @@ from fastapi import APIRouter, Request, Form, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlmodel import Session, select
-from typing import List, Optional
-from database import create_db_and_tables, get_session, engine
+from database import get_session
 from schema import Patient, Doctor
+from datetime import date
 
 router =APIRouter()
 
 templates = Jinja2Templates(directory="static")
 
 
+# HOME PAGE
+
 @router.get("/", name="home", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("home.html",{"request":request})
 
+@router.get("/Form", name="create_form", response_class=HTMLResponse)
+def get_form(request:Request):
+    return templates.TemplateResponse("form.html", {"request":request})
+
+
+# DASHBOARD
+
+
 @router.get("/Dashboard", name="dash_board", response_class=HTMLResponse)
 def details(request: Request, session: Session = Depends(get_session)):
-    patients_count = session.exec(select(Patient)).count()
-    doctors_count = session.exec(select(Doctor)).count()
+    patients_count = len(session.exec(select(Patient)).all())
+    doctors_count = len(session.exec(select(Doctor)).all())
 
     return templates.TemplateResponse("dashboard.html", {"request":request, "patients":patients_count, "doctors":doctors_count})
 
-@router.get("/Patients", response_class=HTMLResponse)
+
+# PATIENTS AND DOCTORS DETAILS 
+
+
+@router.get("/Patients", name="patient_details", response_class=HTMLResponse)
 def get_patients(request: Request, session: Session = Depends(get_session)):
     patients = session.exec(select(Patient)).all()
 
     return templates.TemplateResponse("patients.html", {"request":request, "patients":patients})
+
+@router.get("/Doctors", name="doctor_details", response_class=HTMLResponse)
+def get_doctors(request:Request, session: Session = Depends(get_session)):
+    doctors = session.exec(select(Doctor)).all()
+
+    return templates.TemplateResponse("doctors.html", {"request":request, "doctors":doctors})
+
+
+# CREATE PATIENT DETAIL
+
+
+@router.post("/patients/create", response_class=HTMLResponse)
+def create_patient_form_post(request: Request, name: str = Form(...), age: int = Form(...), gender: str = Form(...),
+                             phone: str = Form(...), place: str = Form(...), admission_date: date = Form(...), 
+                             doctor_name: str = Form(...), status: str = Form(...), session: Session = Depends(get_session)):
+        
+    db_patient = Patient(
+        name=name,
+        age=age,
+        gender=gender,
+        phone=phone,
+        place=place,
+        admission_date=admission_date,
+        doctor_name=doctor_name,
+        status=status)
+    
+    session.add(db_patient)
+    session.commit()
+    session.refresh(db_patient)
+    patients = session.exec(select(Patient)).all()
+    return templates.TemplateResponse("patients.html", {"request": request, "patients":patients})
